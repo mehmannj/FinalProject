d8a92e3d5def0d446c8345fc37c0a9fb30acb0fa
Mann Mehta <mann@llamachant.com>
2025-12-01 04:41:46 -0500

    created map view lifecyclye kt

---
+++ app/src/main/java/week11/st185898/finalproject/ui/rememberMapViewWithLifecycle.kt
@@ -0,0 +1,27 @@
+package week11.st185898.finalproject.ui
+
+import android.os.Bundle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.gms.maps.MapView
+
+@Composable
+fun rememberMapViewWithLifecycle(): MapView {
+    val context = LocalContext.current
+    val mapView = remember { MapView(context) }
+
+    DisposableEffect(Unit) {
+        mapView.onCreate(Bundle())
+        mapView.onStart()
+        mapView.onResume()
+
+        onDispose {
+            mapView.onPause()
+            mapView.onStop()
+            mapView.onDestroy()
+        }
+    }
+    return mapView
+}
